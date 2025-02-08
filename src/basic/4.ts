9d13bfd7e03260a2e30ba4a1bd7e17de3655b0bb
Yuliya Bondarenko <juliyaodessa84@gmail.com>
2025-02-08 21:42:20 +0100

    ts-4

---
+++ src/basic/4.ts
@@ -0,0 +1,15 @@
+function showMessage(message: string): void {
+  console.log(message);
+}
+showMessage("Hello World");
+
+function calc(num1: number, num2: number): number {
+  return num1 + num2;
+}
+console.log(calc(5, 5));
+
+function customError(): never {
+  throw new Error("Error");
+}
+
+export default { customError, calc, showMessage };
