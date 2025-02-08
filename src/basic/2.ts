33a4c34f28b61ee178482c04bf5a67dfd1e58031
Yuliya Bondarenko <juliyaodessa84@gmail.com>
2025-02-08 21:02:27 +0100

    1-2-3-done

---
+++ src/basic/2.ts
@@ -0,0 +1,6 @@
+let person: [string, number];
+
+person = ["Max", 21];
+console.log(person);
+
+export default person;
---
+++ src/basic/3.ts
@@ -0,0 +1,11 @@
+let unionType: string | number;
+unionType = "string";
+
+console.log(unionType);
+
+type literalType = "enable" | "disable";
+
+let value: literalType;
+value = "enable";
+value = "disable";
+console.log(value);
